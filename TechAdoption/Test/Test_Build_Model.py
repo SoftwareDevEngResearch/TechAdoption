d406a5557e49d8ec963f4a7c053e7dd3b8c71300
Epeiffer1 <peiffer.erin@gmail.com>
2020-06-05 12:21:39 -0500

    Test model now runs 10 tests on Build_Model.py file

--- TechAdoption/Test/Test_Build_Model.py
+++ TechAdoption/Test/Test_Build_Model.py
@@ -1,135 +1,104 @@
-""" Created by: Erin Peiffer, 12 May 2020 
+""" Created by: Erin Peiffer, June 2020 
 
 """
-from Build_Model import (format_magpi, format_dataset, split_train_test, create_random_forest, predict_test_data, 
+from ..Build_Model import (format_magpi, format_dataset, split_train_test, create_random_forest, predict_test_data, 
 evaluate_fit, list_top_features, plot_top_features, plot_predicted_actual, plot_tree)
-import pytest
 import pandas as pd
-import csv
+import numpy as np
 
 
-def test_format_magpi1():
-	''' Make sure that number of columns are correct '''
-	data = 'testMagpi.csv'
-	exp_df = 6
-	questions = 2
-	options = 2
-	obs_df, obs_options = format_magpi(data,options,questions)
-	assert exp_df == len(obs_df.columns)
-
-def test_format_magpi2():
-	''' Make sure that the number of options/devices are correct '''
-	data = 'testMagpi.csv'
-	questions = 2
-	exp_options = 2
-	obs_df, obs_options = format_magpi(data,options,questions)
-	assert exp_options == len(obs_options)	
+""" df for format_dataset, split_train_test """
+df = pd.DataFrame()
+df['Devices'] = ['a','b','c','d','e','f','g','h','i']
+cols = ['b','c','d','e','f','g']
+for col in cols:
+	df[col] = [1,2,3,4,5,6,7,8,9]
 
 def test_format_dataset1():		
-	''' Make sure features are loaded correctly using dummy dataset'''
-	exp_features = [[0]*6, [1]*6, [2]*6, [3]*6, [4]*6, [5]*6, [6]*6,[7]*6,[8]*6]
-	data = 'testdata.csv'
-	obs_features, obs_labels, obs_feature_list = test_format_dataset(data)
-	assert exp_features == obs_features
+	"""Make sure features are loaded correctly using dummy dataset"""
+	exp_features = [[1]*6, [2]*6, [3]*6, [4]*6, [5]*6, [6]*6,[7]*6,[8]*6,[9]*6]
+	exp_features = np.array(exp_features)
+	obs_features, obs_labels, obs_feature_list = format_dataset(df)
+	assert np.all(exp_features == obs_features)
 	
 def test_format_dataset2():		
-	''' Make sure labels are loaded correctly using dummy dataset'''
-	exp_labels = [0 1 2 3 4 5 6 7 8]
-	data = 'testdata.csv'
-	obs_features, obs_labels, obs_feature_list = test_format_dataset(data)
-	assert exp_lables == obs_labels
+	""" Make sure labels are loaded correctly using dummy dataset"""
+	exp_labels = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+	obs_features, obs_labels, obs_feature_list = format_dataset(df)
+	assert np.all(exp_labels == obs_labels)
 
 def test_format_dataset3():		
-	''' Make sure feature list is loaded correctly using dummy dataset'''
+	""" Make sure feature list is loaded correctly using dummy dataset"""
 	exp_feature_list = ['b', 'c', 'd', 'e', 'f', 'g']
-	data = 'testdata.csv'
-	obs_features, obs_labels, obs_feature_list = test_format_dataset(data)
-	assert exp_feature_list == obs_feature_list
-
-def test_split_train_test1():
-	''' Make sure the shape of the train and test features (x) data sets are the same '''
-	data = 'testdata.csv'
-	df = pd.read_csv(data)
-	obs_features = df[df.columns[0]]
-	obs_labels = df[df.columns[1:6]]
-	train_features, train_labels, test_features, test_labels = split_train_test(obs_features, obs_labels, 0.25, 42)
-	assert train_features.shape[1] == test_features.shape[1]
+	obs_features, obs_labels, obs_feature_list = format_dataset(df)
+	assert np.all(exp_feature_list == obs_feature_list)
 
 def test_split_train_test2():
-	''' Make sure the shape of the train and test labels (y) data sets are the same '''
-	data = 'testdata.csv'
-	df = pd.read_csv(data)
+	""" Make sure the shape of the train and test labels (y) data sets are the same """
 	obs_features = df[df.columns[0]]
 	obs_labels = df[df.columns[1:6]]
 	train_features, train_labels, test_features, test_labels = split_train_test(obs_features, obs_labels, 0.25, 42)
 	assert train_labels.shape[1] == test_labels.shape[1]
 
-''' Not sure how to test the below function '''
-def test_create_random_forest():
+def test_predict_test_data():
+	""" Make sure the length of predictions matches the length of test_labels """
 	trees = 1000
 	random_state = 42
 	maxfeatures = float(1/3)
-	data = 'testdata.csv'
-	df = pd.read_csv(data)
-	features = df[df.columns[0]]
-	labels = df[df.columns[1:6]]
-	rf = RandomForestClassifier(n_estimators = trees, random_state = randomstate, max_features = maxfeatures)
-	rf.fit(features, labels)
-	return rf
-
-def test_predict_test_data():
-	''' Make sure the length of predictions matches the length of test_labels ''' 
-	data = 'testdata.csv'
-	df = pd.read_csv(data)
-	labels = df[df.columns[1:6]]
-	features = df[df.columns[0]]
-	predictions = rf.predict(features)
+	features =  [[1]*6, [2]*6, [3]*6, [4]*6, [5]*6, [6]*6,[7]*6,[8]*6,[9]*6]
+	features = np.array(features)
+	labels = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+	rf = create_random_forest(trees, random_state, maxfeatures, features, labels)
+	predictions = predict_test_data(rf, features)
 	assert len(predictions) == len(labels)
-	
+
 def test_evaluate_fit1():
-	''' Test errors for perfect prediction '''
-	predictions = [1,1,1,1,1]
-	actual = [1,1,1,1,1]
+	""" Test errors for perfect prediction """
+	predictions = np.array([1,1,1,1,1])
+	actual = np.array([1,1,1,1,1])
 	obs_errors, obs_accuracy = evaluate_fit(predictions, actual)
 	exp_errors = 0
 	exp_accuracy = 100
-	assert exp_errors = obs_errors
-	assert exp_accuracy = obs_accuracy
+	assert np.all(exp_errors == obs_errors)
 	
 def test_evaluate_fit2():
-	''' Test errors for worst fit prediction '''
-	predictions = [0,0,0,0,0]
-	actual = [1,1,1,1,1]
+	""" Test errors for worst fit prediction """
+	predictions = np.array([0,0,0,0,0])
+	actual = np.array([1,1,1,1,1])
 	obs_errors, obs_accuracy = evaluate_fit(predictions, actual)
-	exp_errors = len(predictions) # WHAT SHOULD THIS NUMBER BE??
-	assert exp_errors = obs_errors
+	print('OBS = ',obs_errors)
+	exp_errors = [1,1,1,1,1]
+	assert np.all(exp_errors == obs_errors)
 
 def test_evaluate_fit3():
-	''' Test accuracy for perfect prediction '''
-	predictions = [1,1,1,1,1]
-	actual = [1,1,1,1,1]
+	""" Test accuracy for perfect prediction """
+	predictions = np.array([1,1,1,1,1])
+	actual = np.array([1,1,1,1,1])
 	obs_errors, obs_accuracy = evaluate_fit(predictions, actual)
 	exp_accuracy = 100
-	assert exp_accuracy = obs_accuracy
+	assert exp_accuracy == obs_accuracy
 
 def test_evaluate_fit4():
-	''' Test accuracy for worst fit prediction '''
-	predictions = [0,0,0,0,0]
-	actual = [1,1,1,1,1]
+	""" Test accuracy for worst fit prediction """
+	predictions = np.array([0,0,0,0,0])
+	actual = np.array([1,1,1,1,1])
 	obs_errors, obs_accuracy = evaluate_fit(predictions, actual)
 	exp_accuracy = 0
-	assert exp_accuracy = obs_accuracy
+	assert exp_accuracy == obs_accuracy
 
 	
-def test_list_top_features(rf, feature_list):	
-	''' Make sure importances are sorted in descending order '''
-	
-	data = 'testdata.csv'
-	df = pd.read_csv(data)
+def test_list_top_features():	
+	""" Make sure importances are sorted in descending order """
+	trees = 1000
+	random_state = 42
+	maxfeatures = float(1/3)
+	features =  [[1]*6, [2]*6, [3]*6, [4]*6, [5]*6, [6]*6,[7]*6,[8]*6,[9]*6]
+	features = np.array(features)
+	labels = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+	rf = create_random_forest(trees, random_state, maxfeatures, features, labels)
 	features_list = list(df.columns)	
 	importances = list_top_features(rf,features_list)
 	assert importances[0] >= importances[-1]
-
 
 
 
